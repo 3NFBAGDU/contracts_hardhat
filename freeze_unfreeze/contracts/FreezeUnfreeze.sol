899bf5e4e7d132612cd819b20c7903243036b2f6
3NFBagdu <gbagh16@freeuni.edu.ge>
2022-05-02 14:47:26 +0400

    feat: add freeze unfreeze functionality

---
+++ freeze_unfreeze/contracts/FreezeUnfreeze.sol
@@ -0,0 +1,77 @@
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract FreezeUnfreeze is ERC20, Ownable, Pausable{
+
+    using SafeMath for uint256;
+    using Counters for Counters.Counter;
+
+    // freeze accounts mapping
+    mapping(address=>bool) freezed;
+
+    // mapping token and its owner
+    mapping(uint256=>address) tokenOwner;
+
+
+    // cost mint
+    uint256 public cost = 1;
+
+    // token counter
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC20("MyToken", "MTK") {}
+
+    function freeze(address[] calldata addrs) external onlyOwner returns(bool){
+        for (uint256 i = 0; i < addrs.length; ++ i) {
+            freezed[addrs[i]] = true;
+        }
+        return true;
+    }
+
+    function unFreeze(address[] calldata addrs) external onlyOwner {
+        for (uint256 i = 0; i < addrs.length; ++ i) {
+            delete freezed[addrs[i]];
+        }
+    }
+
+
+    // modifier to give access on some actions only not freezed accounts
+    modifier whenNotFreezed() {
+        require(freezed[msg.sender]==false);
+        _;
+    }
+
+
+    function mint() external payable whenNotPaused whenNotFreezed {
+        require(
+            msg.value >= cost, 
+            "Not enough ether to purchase NFTs."
+        );
+
+        uint256 newTokenID = _tokenIds.current();
+        _mint(msg.sender, 1);
+        tokenOwner[newTokenID] = msg.sender;
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+}
--- freeze_unfreeze/test/test_freeze_unfreeze.js
+++ freeze_unfreeze/test/test_freeze_unfreeze.js
@@ -1,2 +1,66 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+
+
+describe("Test Mint", function () {
+  it("Test Mint", async function () {
+    const [owner, addr1] = await ethers.getSigners();
+
+    const freezeUnfreeze = await ethers.getContractFactory("FreezeUnfreeze");
+
+    const hardhatToken = await freezeUnfreeze.deploy();
+
+    await hardhatToken.connect(addr1).mint({
+      value: 1
+    })
+    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(1);
+
+  });
+})
+
+describe("Test pause", function () {
+  it("Test Pause", async function () {
+      const [owner, addr1] = await ethers.getSigners();
+
+      const freezeUnfreeze = await ethers.getContractFactory("FreezeUnfreeze");
+  
+      const hardhatToken = await freezeUnfreeze.deploy();
+
+      // test pause
+      await hardhatToken.pause();
+      await expect(hardhatToken.connect(addr1).mint({
+        value: 1
+      })).to.be.reverted;        
+
+      // test unpause 
+      await hardhatToken.unpause();
+      await hardhatToken.connect(addr1).mint({
+        value: 1
+      })
+      expect(await hardhatToken.balanceOf(addr1.address)).to.equal(1);
+  });
+});
+
+
+describe("Test Freeze/Unfreeze", function () {
+  it("Test Freeze/Unfreeze", async function () {
+      const [owner, addr1, addr2] = await ethers.getSigners();
+
+      const freezeUnfreeze = await ethers.getContractFactory("FreezeUnfreeze");
+  
+      const hardhatToken = await freezeUnfreeze.deploy();
+
+      // test freeze
+      await hardhatToken.freeze([addr1.address]);
+      await expect(hardhatToken.connect(addr1).mint({
+        value: 1
+      })).to.be.reverted;        
+
+      // test freeze
+      await hardhatToken.unFreeze([addr1.address]);
+      await hardhatToken.connect(addr1).mint({
+        value: 1
+      })
+      expect(await hardhatToken.balanceOf(addr1.address)).to.equal(1);
+  });
+});
