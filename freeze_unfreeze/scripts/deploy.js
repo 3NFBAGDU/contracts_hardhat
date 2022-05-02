f9e0a03dfacd61a7f061b63f94162c6b4cef0a30
3NFBagdu <gbagh16@freeuni.edu.ge>
2022-05-02 15:14:28 +0400

    feat: add deploy

--- freeze_unfreeze/scripts/deploy.js
+++ freeze_unfreeze/scripts/deploy.js
@@ -1,4 +1,15 @@
 async function main() {
+  const [deployer] = await ethers.getSigners();
+  
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const Token20 = await ethers.getContractFactory("FreezeUnfreeze");
+  const token20 = await Token20.deploy();
+
+  await token20.mint(deployer.address, 2)
+  console.log("Token address:", token20.address);
 
 }
 
