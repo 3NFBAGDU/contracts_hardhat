f234ec3bfd5ac2f0158ede8974e83eca75ea1b60
3NFBagdu <gbagh16@freeuni.edu.ge>
2022-05-02 14:48:28 +0400

    ref: delete unused var

--- freeze_unfreeze/contracts/FreezeUnfreeze.sol
+++ freeze_unfreeze/contracts/FreezeUnfreeze.sol
@@ -4,11 +4,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract FreezeUnfreeze is ERC20, Ownable, Pausable{
 
-    using SafeMath for uint256;
     using Counters for Counters.Counter;
 
     // freeze accounts mapping
